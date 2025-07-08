11f577a9919e35d98bb9b55993d6f0d6216596fe
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2025-07-08 14:21:11 +0200

    add counter

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+let counter = 0;
+
+setInterval(() => {
+  counter++;
+}, 1000);
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+}).get('/counter', (req, res) => {
+  res.send(`Counter value: ${counter}`);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
