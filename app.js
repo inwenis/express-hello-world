a774942bf3fac726aecd7d86aea56d4fc5a5800e
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2025-07-26 11:22:57 +0200

    disable counter because I don't want to pay for it

--- app.js
+++ app.js
@@ -5,15 +5,15 @@ const port = process.env.PORT || 3000
 
 let counter = 0;
 
-setInterval(async () => {
-  counter++;
-  try {
-    const resp = await axios.get('https://express-hello-world-3zfl.onrender.com/counter');
-    // console.log(`Counter updated: ${counter}, Response from server: ${resp.data}`);
-  } catch (error) {
-    console.error(`Error fetching counter: ${error.message}`);
-  }
-}, 1000);
+// setInterval(async () => {
+//   counter++;
+//   try {
+//     const resp = await axios.get('https://express-hello-world-3zfl.onrender.com/counter');
+//     // console.log(`Counter updated: ${counter}, Response from server: ${resp.data}`);
+//   } catch (error) {
+//     console.error(`Error fetching counter: ${error.message}`);
+//   }
+// }, 1000);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
